b1a5309868c56224d68c6872b0201ac60478eaf7
Jalon Rivers <jrivers9579@floridapoly.edu>
2026-01-14 19:44:46 +0000

    hw 1

---
+++ homework/HW_01_print_test.py
@@ -0,0 +1,8 @@
+my_name ="jalon rivers"
+print(my_name)
+print("Use 4 spaces per indentation level.")
+print("Python has only one type of module object, and all modules are of this type," \
+"regardless of whether the module is implemented in Python, C, or something else.")
+print("three outcomes I hope to gain from taking this course is to improve my overall coading skills, " \
+    "be able to take on a responsibility that i will have to take on soon for a club," \
+    " and be able to use python for personal projects")
